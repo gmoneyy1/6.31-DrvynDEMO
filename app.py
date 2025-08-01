d9ace45d0e2c7b1202405d82e3e794b32007c7ae
gmoneyy1 <georgezhan2009@gmail.com>
2025-07-31 22:43:11 -0700

    Force Render redeploy

--- app.py
+++ app.py
@@ -515,3 +515,4 @@ if __name__ == "__main__":
     debug = os.environ.get('FLASK_ENV') == 'development'
     
     app.run(debug=debug, port=port, host='0.0.0.0') # Heroku deployment test
+# Force Render redeploy
